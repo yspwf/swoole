402d0c2d0cda5988b1ff27faed218380ca25264d
yspwf <846163966@qq.com>
2018-10-16 18:02:58 +0800

    client

--- http_client.php
+++ http_client.php
@@ -1,6 +1,6 @@
 <?php 
 
-$client = new swoole_client(SWOOLE_TCP_SOCK);
+$client = new swoole_client(SWOOLE_SOCK_TCP,SWOOLE_SOCK_ASYNC);
 
 if(!$client->connect('192.168.114.128',9503,-1)){
 	exit("connect failed , Error:{$client->errCode} \r\n");
