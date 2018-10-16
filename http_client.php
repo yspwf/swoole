8dc07547eb1f25c7e9e39a30e168f167e72933d9
yspwf <846163966@qq.com>
2018-10-16 18:16:42 +0800

    compare

--- http_client.php
+++ http_client.php
@@ -1,12 +1,27 @@
 <?php 
 
 $client = new swoole_client(SWOOLE_SOCK_TCP);
+
+if(!$client->connect('192.168.114.128',9503,-1)){
+	exit("connect failed , Error:{$client->errCode} \r\n");
+}
+
+$client->send("cleint send data \r\n");
+
+echo $client->recv();
+
+$client->close();
+
+
+/*
+$client = new swoole_client(SWOOLE_SOCK_TCP);
 if (!$client->connect('192.168.114.128', 9503, -1))
 {
     exit("connect failed. Error: {$client->errCode}\n");
 }
-$client->send("hello world\n");
+$client->send("cleint send data \r\n");
 echo $client->recv();
 $client->close();
+*/
 
 ?>
