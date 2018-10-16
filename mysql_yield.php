daeb1080c57b70026c9521ee41f0dd7b7ddd7b59
yspwf <846163966@qq.com>
2018-10-16 16:56:20 +0800

    mysql connect

--- mysql_yield.php
+++ mysql_yield.php
@@ -11,8 +11,28 @@ $server->on('connect', function($serv){
 });
 
 $server->on('request', function($req, $res){
-	$res->write("hello swoole, http request!!");
-	$res->end();
+	
+	$mysql = new Swoole\Coroutine\MySql();
+
+	$db = array(
+		'host'=>'127.0.0.1',
+		'user'=>'root',
+		'password'=>'123456',
+		'database'=>'demo'
+	);
+
+	$conn = $mysql->connect($db);
+	if($conn == false){
+		$res->end("mysql connect fail!!");
+		return;
+	}
+
+	$dbres = $mysql->query("select 1+1");
+
+	$res->end("swoole response is ok, result=".var_dump($dbres, true));
+
+	//$res->write("hello swoole, http request!!");
+	///$res->end();
 });
 
 $server->start();
