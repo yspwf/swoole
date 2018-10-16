8dcdf9194fc4d64e921059c794b40782b7473155
yspwf <846163966@qq.com>
2018-10-16 16:49:25 +0800

    mysql test

---
+++ mysql_yield.php
@@ -0,0 +1,20 @@
+<?php 
+
+$server = new Swoole\Http\Server('127.0.0.1', 9502, SWOOLE_BASE);
+
+$server->set(array(
+	'worker_num' => 1
+));
+
+$server->on('connect', function($serv){
+	echo "http server start";
+});
+
+$server->on('request', function($req, $res){
+	$res->write("hello swoole, http request!!");
+	$res->end();
+});
+
+$server->start();
+
+?>
