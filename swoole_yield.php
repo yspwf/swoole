3bc44d8004f9d4e5ddee79f2f55ab3bb037c1938
yspwf <846163966@qq.com>
2018-10-16 14:53:54 +0800

    port one

--- swoole_yield.php
+++ swoole_yield.php
@@ -1,9 +1,9 @@
 <?php 
 
-$http = new swoole_http_server("127.0.0.1", 9502);
+$http = new swoole_http_server("0.0.0.0", 9502);
 
 $http->on("start", function($serv){
-	echo "swoole http server is started at http://127.0.0.1:9053".PHP_EOL;
+	echo "swoole http server is started at http://127.0.0.1:9502".PHP_EOL;
 });
 
 $http->on("request", function($req, $res){
